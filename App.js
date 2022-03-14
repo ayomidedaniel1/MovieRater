da53a19c5f030183f2cd0ca432d8886a843101f8
Ayomide <ayomidedans@gmail.com>
2022-03-14 01:58:46 +0100

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import RootNavigator from './navigation/RootNavigator';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import moviesReducer from './reducers';
+
+const rootReducer = combineReducers({
+  moviesReducer,
+});
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
